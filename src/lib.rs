ee34ed47c6a72ef83243ec7dac17dcc49e3214a1
masaru514 <415urasam@gmail.com>
2021-08-15 17:34:20 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -24,15 +24,56 @@ pub mod minigrep {
         }
     }
 
-    pub fn run(config: Config) -> Result<(), Box<Error>> {
-        let mut f = File::open(config.filename).expect("file not found");
+    pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+      let mut results = Vec::new();
+
+      for line in contents.lines() {
+        if line.contains(query) {
+          results.push(line)
+        }
+      }
+
+      results
+    }
+
+    pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+        let mut f = File::open(config.filename)?;
 
         let mut contents = String::new();
-        f.read_to_string(&mut contents).expect("something went wront reading the file");
+        f.read_to_string(&mut contents)?;
 
-        println!("With text:\n{}", contents);
+        for line in search(&config.query, &contents) {
+          println!("{}", line);
+        }
 
         Ok(())
     }
+}
 
+mod test {
+  #[allow(dead_code)]
+  fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+      if line.contains(query) {
+        results.push(line)
+      }
+    }
+
+    results
+  }
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+    assert_eq!(
+      vec!["safe, fast, productive."],
+      search(query, contents)
+    );
+  }
 }
