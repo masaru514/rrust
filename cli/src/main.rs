49fcc2d91cfa71379381d9a3b45b06c8c4d8b0c4
masaru514 <415urasam@gmail.com>
2021-08-15 20:33:26 +0900

    chore

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application Erorr!: {}", e);
+        eprintln!("Application Erorr!: {}", e);
         process::exit(1);
     }
 }
