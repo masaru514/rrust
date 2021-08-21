048c349498afb1c29f6ba3c7d8d95864af16c620
masaru514 <415urasam@gmail.com>
2021-08-21 23:32:11 +0900

    update

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,17 +1,71 @@
-use std::env;
-use std::process;
-use rrust::minigrep;
+use std::thread;
+use std::time::Duration;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
+fn generate_workout(intensity: u32, random_number: u32) {
+    struct Cacher<T> where T: Fn(u32) -> u32 {
+        calculation: T,
+        value: Option<u32>
+    }
+
+    impl<T> Cacher<T> where T: Fn(u32) -> u32 {
+        fn new(calculation: T) -> Cacher<T> {
+            Cacher {
+                calculation,
+                value: None,
+            }
+        }
+
+        fn value(&mut self, arg: u32) -> u32 {
+            match self.value {
+                Some(v) => v,
+                None => {
+                    let v = (self.calculation)(args);
+                    self.value = Some(v);
+                    v
+                }
+            }
+        }
+    }
+
+    let expensive_result = Cacher::new(|num| {
+        println!("calcu slow...");
+        thread::sleep(Duration::from_secs(2));
+        num
     });
 
-    if let Err(e) = minigrep::run(config) {
-        eprintln!("Application Erorr!: {}", e);
-        process::exit(1);
+    if intensity < 25 {
+        println!(
+            // 今日は{}回腕立て伏せをしてください！
+            "Today, do {} pushups!",
+            expensive_closure(intensity)
+        );
+
+        println!(
+            // 次に、{}回腹筋をしてください！
+            "Next, do {} situps!",
+            expensive_closure(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            // 今日は休憩してください！水分補給を忘れずに！
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                // 今日は、{}分間走ってください！
+                "Today, run for {} minutes!",
+                expensive_closure(intensity)
+            );
+        }
     }
+}
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
 }
--- heep/src/main.rs
+++ heep/src/main.rs
@@ -1,13 +1,24 @@
-enum List {
-  Cons(i32, Box<List>),
-  Nil,
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+  type Target = T;
+  fn deref(&self) -> &T {
+    &self.0
+  }
 }
 
-use List::{Cons, Nil};
+struct MyBox<T>(T);
 
-fn main() {
-  let list = Cons(1,
-    Box::new(Cons(2,
-      Box::new(Cons(3,
-        Box::new(Nil))))));
+impl<T> MyBox<T> {
+  fn new(x: T) -> MyBox<T> {
+    MyBox(x)
+  }
+}
+
+fn main () {
+  let x = 5;
+  let y = MyBox::new(x);
+
+  assert_eq!(5, x);
+  assert_eq!(5, *y);
 }
